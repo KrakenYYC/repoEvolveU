7e6f73e9b97937d48ef782cf5a27c4f550a93bf8
KrackenYYC <44476543+KrackenYYC@users.noreply.github.com>
2019-04-13 09:02:07 -0600

    April 13, 2019

---
+++ react-cln/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './App.css';
+
+import zIntro from './zIntro';
+
+
+function First () {
+
+    return (
+    <h1>hello</h1>
+
+  }
+}
+
+export default First;
---
+++ react-cln/src/zIntro.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function zIntro () {
+	<div>
+	<h1>HELLO WORLD</h1>
+	</div>
+}
+
+export default zIntro
