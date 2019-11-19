0f83bbaaeea65087f429512783f92e83374fab41
Carrie Nermo <47464668+KrakenYYC@users.noreply.github.com>
2019-11-18 19:53:21 -0700

    updates November

---
+++ Python/HelloWorld.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 17:20:51 2019
+
+@author: carcl
+"""
+
+print ("Hello World")
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+    
+def marble():
